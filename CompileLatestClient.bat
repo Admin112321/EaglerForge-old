8b6ca1e7fe400346ad7cdadb682ed0ddaded391e
Admin112321 <97298978+Admin112321@users.noreply.github.com>
2025-05-22 08:52:44 -0400

    Update CompileLatestClient.bat

--- CompileLatestClient.bat
+++ CompileLatestClient.bat
@@ -1,6 +1,3 @@
 @echo off
-title CompileLatestClient
-java -cp "buildtools/BuildTools.jar" net.lax1dude.eaglercraft.v1_8.buildtools.gui.CompileLatestClientGUI
-del /S /Q "##TEAVM.TMP##\*"
-rmdir /S /Q "##TEAVM.TMP##"
-pause
+title CompileLatestClient (No GUI)
+java -cp "buildtools/BuildTools.jar" net.lax1dude.eaglercraft.v1_8.buildtools.cli.CompileLatestClient
